e59f4055ae29f06ff4c85f7a32d321334c7d324c
khm <klaus.h.matzka@gmail.com>
2018-10-21 15:57:44 +0200

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Klaus Matzka on 21.10.18.
+//  Copyright © 2018 Klaus Matzka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
