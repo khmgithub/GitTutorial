45fcdd84c2916dd4c919c25c841a50875c0dadcd
khm <klaus.h.matzka@gmail.com>
2018-10-21 17:01:42 +0200

    doch wieder ein print statement

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Hallo Welt!")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
